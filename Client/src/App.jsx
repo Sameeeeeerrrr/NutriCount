b1d9a812f1b5a4a871370a85b604f454caf74604
Sameeeeeerrrr <161756041+Sameeeeeerrrr@users.noreply.github.com>
2024-05-16 13:31:15 +0530

    Add files via upload

---
+++ Client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { LoginPage } from "./Components/LoginPage"
+import { SigupPage } from "./Components/SignupPage"
+import { ServicesPage } from "./Components/ServicesPage/ServicesPage"
+import { ApiPage } from "./Components/ApiPage/ApiPage"
+import { AddItemPage } from "./Components/ServicesPage/AddItemsPage"
+
+export const LINK = "http://localhost:8080"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SigupPage />}></Route>
+        <Route path="/services" element={<ServicesPage />}></Route>
+        <Route path="/api" element={<ApiPage />}></Route>
+        <Route path="/services/add" element={<AddItemPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ Client/src/Components/Topbar.jsx
@@ -0,0 +1,5 @@
+export function TopBar(){
+    return <div>
+        <h2>Nutrition App</h2>
+    </div>
+}
